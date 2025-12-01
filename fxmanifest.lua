b02cc1dd82babf3abc1d33af9b61ed5b1f942021
kyleb500003 <72032190+alvwashere@users.noreply.github.com>
2025-12-01 00:58:00 +0000

    Create resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Alv'
+description 'Standalone FiveM loading screen with minimalistic UI.'
+url 'github.com/alvwashere'
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/loading.mp4',
+    'html/music/*.mp3',
+}
+
+loadscreen_cursor 'yes'
